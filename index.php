33ad60dad590a897a634fa6cc4fe4e65db17ee58
edgarmc <edgarmc11@hotmail.com>
2020-12-01 22:25:01 -0500

    version3

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Sistema escolar</title>
 </head>
 <body>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet maxime facere explicabo, ut dolor quaerat quo iusto a autem animi culpa qui delectus, reprehenderit atque voluptas mollitia fugit eveniet optio!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet maxime facere explicabo, ut dolor quaerat quo iusto a autem animi culpa qui delectus, reprehenderit atque voluptas mollitia fugit eveniet optio!git a</p>
 </body>
 </html>
